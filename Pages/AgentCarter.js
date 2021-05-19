27b37c1b3cbe8b81683216d74608692eb3ae54c6
mhcdn <80976618+mhcdn@users.noreply.github.com>
2021-05-18 22:51:25 -0700

    Create AgentCarter.js

---
+++ Pages/AgentCarter.js
@@ -0,0 +1,28 @@
+myObject = new Vue({
+  el: &#39;#agentcarter&#39;,
+  data: {
+    seasonone: [
+      { name: 'Episode 1', url: '&ep=1', picurl: 'S1/ep01.webp', date: 'Jan 6, 2015', time: '55min'},
+      { name: 'Episode 2', url: '&ep=2', picurl: 'S1/ep02.webp', date: 'Jan 6, 2015', time: '55min'},
+      { name: 'Episode 3', url: '&ep=3', picurl: 'S1/ep03.webp', date: 'Jan 13, 2015', time: '55min'},
+      { name: 'Episode 4', url: '&ep=4', picurl: 'S1/ep04.webp', date: 'Jan 27, 2015', time: '55min'},
+      { name: 'Episode 5', url: '&ep=5', picurl: 'S1/ep05.webp', date: 'Feb 03, 2015', time: '55min'},
+      { name: 'Episode 6', url: '&ep=6', picurl: 'S1/ep06.webp', date: 'Feb 10, 2015', time: '55min'},
+      { name: 'Episode 7', url: '&ep=7', picurl: 'S1/ep07.webp', date: 'Feb 17, 2015', time: '55min'},
+      { name: 'Episode 8', url: '&ep=8', picurl: 'S1/ep08.webp', date: 'Feb 24, 2015', time: '55min'}
+            ],
+    seasontwo: [
+      { name: 'Episode 1', url: '&ep=1', picurl: 'S2/ep01.webp', date: 'Jan 19, 2016', time: '55min'},
+      { name: 'Episode 2', url: '&ep=2', picurl: 'S2/ep02.webp', date: 'Jan 19, 2016', time: '55min'},
+      { name: 'Episode 3', url: '&ep=3', picurl: 'S2/ep03.webp', date: 'Jan 26, 2016', time: '55min'},
+      { name: 'Episode 4', url: '&ep=4', picurl: 'S2/ep04.webp', date: 'Feb 2, 2016', time: '55min'},
+      { name: 'Episode 5', url: '&ep=5', picurl: 'S2/ep05.webp', date: 'Feb 9, 2016', time: '55min'},
+      { name: 'Episode 6', url: '&ep=6', picurl: 'S2/ep06.webp', date: 'Feb 16, 2016', time: '55min'},
+      { name: 'Episode 7', url: '&ep=7', picurl: 'S2/ep07.webp', date: 'Feb 16, 2016', time: '55min'},
+      { name: 'Episode 8', url: '&ep=8', picurl: 'S2/ep08.webp', date: 'Feb 23, 2016', time: '55min'},
+      { name: 'Episode 9', url: '&ep=9', picurl: 'S2/ep09.webp', date: 'Feb 23, 2016', time: '55min'},
+      { name: 'Episode 10', url: '&ep=10';, picurl:'S2/ep10.webp', date: 'March 1, 2016', time: '55min'}
+            ]
+  }
+})
+
